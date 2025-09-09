01d8b853256f02a57a2f9695556e2185484cc1eb
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2025-09-09 12:25:42 +0530

    Fix payout flow simulation script

--- scripts/payout-flow-simulation.js
+++ scripts/payout-flow-simulation.js
@@ -120,7 +120,7 @@ async function main() {
     const wrappedToken = await ethers.getContractAt("WRAPEDTOKEN", wrappedTokenAddress);
     
     // Grant payout admin role to payoutAdmin
-    const PAYOUT_ADMIN_ROLE = await wrappedToken.PAYOUT_ADMIN_ROLE();
+    const PAYOUT_ADMIN_ROLE = await wrappedToken.PAYOUT_ADMIN_ROLE;
     await wrappedToken.connect(deployer).grantRole(PAYOUT_ADMIN_ROLE, payoutAdmin.address);
     
     // Transfer sale tokens to offering for distribution
